9211bc08768dc4f2051fa878730f5f278970a8a8
Wasiu Bakare <wasiubakare@gmail.com>
2021-07-02 01:08:52 +0100

    done with UI

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
+
+const ProductScreen = ({ match }) => {
+    const product = products.find(p => p._id === match.params.id)
+    return (
+        <>
+            <Link to='/' className='btn btn-outline-dark rounded my-3'><i className='fas fa-arrow-left me-2'></i>Go Back</Link>
+            <Row>
+                <Col md={5}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={4}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: &#8358;{product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>&#8358;{product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                <div className="d-grid">
+                                    <Button className='btn-dark rounded' type='button' disabled={product.countInStock === 0}>Add To Cart</Button>
+                                </div>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default ProductScreen
